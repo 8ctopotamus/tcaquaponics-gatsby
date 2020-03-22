588a3d29972807141145189bfb7b484d6786dc5b
8ctopotamus <joshnaylor88@gmail.com>
2020-03-22 11:37:26 -0500

    Rounded title

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,11 +21,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Twin Cities Aquaponics`,
+        short_name: `TC Aquaponics`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#316589`,
+        theme_color: `#316589`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
